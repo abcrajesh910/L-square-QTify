a9b42b92c1489cc519f2b8b6bf53cd28663480dd
Rajesh Singh (EXT-Nokia) <rajesh.singh.ext@nokia.com>
2026-02-17 22:42:36 +0530

    Fixed Navbar alignment and Hero text1

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,9 @@
 import React from "react";
 import LogoImage from "../../assets/logo.png";
-import LogoStyles from "./logo.module.css";
+// import LogoStyles from "./logo.module.css";
 
 const Logo = () => {
-  return <img src={LogoImage} alt="QTify Logo" width={67} className={LogoStyles.logo} />;
+  return <img src={LogoImage} alt="QTify Logo" width={67}  />;
 };
 
 export default Logo;
