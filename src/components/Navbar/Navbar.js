ad6d38b00635f9f81c5d50d427e1746feb0756cc
Rajesh Singh (EXT-Nokia) <rajesh.singh.ext@nokia.com>
2026-02-19 23:01:57 +0530

    Add: functional search bar for Qtify

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,33 @@
 import React from "react";
-import Logo from "../Logo/Logo";
+// import Logo from "../Logo/Logo";
+import LogoImage from "../../assets/logo.png";
 import Search from "../Search/Search";
 import Button from "../Button/Button";
 import styles from "./Navbar.module.css";
 
-function Navbar() {
-  return (
-    <nav className={styles.navbar}>
-      <Logo />
-      <Search placeholder="Search a song of your choice" />
-      <Button>Give Feedback</Button>
-    </nav>
-  );
-}
+// function Navbar() {
+//   return (
+//     <nav className={styles.navbar}>
+//       <Logo />
+//       <Search placeholder="Search a song of your choice" />
+//       <Button>Give Feedback</Button>
+//     </nav>
+//   );
+// }
 
-export default Navbar;
+// export default Navbar;
+
+
+
+
+export default function Navbar(){
+    return(
+        <>
+            <nav className={styles.navbar}>
+                <div className={styles.logoDiv}><img  src={LogoImage} alt="logo" width={67}/></div>
+                 <Search search={"Search a song of your choice"}/>
+                <Button children="Give Feedback"/>
+            </nav>
+        </>
+    )
+}
